f4eb4bc8f42161e59786780da8be1ed1403fc552
Julian Krumow <julian.krumow@artcom.de>
2016-01-25 11:04:40 +0100

    Cleanup code.

--- Project/JSONAPITests/JSONAPITests.m
+++ Project/JSONAPITests/JSONAPITests.m
@@ -180,13 +180,11 @@
     NSDictionary *json = [JSONAPIResourceParser dictionaryFor:newArticle];
     XCTAssertEqualObjects(json[@"type"], @"articles", @"Did not create Article!");
     XCTAssertEqualObjects(json[@"links"][@"self"], @"http://example.com/articles/1", @"Self link should be 'http://example.com/articles/1'!");
-    
     XCTAssertNotNil(json[@"relationships"], @"Did not create links!");
     XCTAssertNotNil(json[@"relationships"][@"author"], @"Did not create links!");
     XCTAssertNotNil(json[@"relationships"][@"author"][@"data"], @"Did not create links!");
     XCTAssertEqualObjects(json[@"relationships"][@"author"][@"data"][@"id"], newAuthor.ID, @"Wrong link ID!.");
     XCTAssertNil(json[@"relationships"][@"author"][@"first-name"], @"Bad link!");
-
     XCTAssertNotNil(json[@"relationships"][@"comments"], @"Did not create links!");
     XCTAssertTrue([json[@"relationships"][@"comments"][@"data"] isKindOfClass:[NSArray class]], @"Comments data should be array!.");
     XCTAssertEqual([json[@"relationships"][@"comments"][@"data"] count], 2, @"Comments should have 2 elements!.");
